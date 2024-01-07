117d522dae3dc843238624924c348b00c94f4f99
sydorovay <sydorovay@gmail.com>
2024-01-07 13:24:06 +0000

    Deploying to gh-pages from @ sydorovay/project-TECHNO12-main-modern@a13e714f12d35b5a2c4626f6ce42ab52723e3bee 🚀

---
+++ shoppinglist.937fbaae.js
@@ -0,0 +1,2 @@
+!function(){var e=window.location.pathname,t=document.querySelector(".home-link"),n=document.querySelector(".shopping-list-link");"/index.html"===e?(t.classList.add("current-page-home"),n.classList.remove("current-page-shopping")):"/shoppinglist.html"===e&&(n.classList.add("current-page-shopping"),t.classList.remove("current-page-home"))}();
+//# sourceMappingURL=shoppinglist.937fbaae.js.map
---
+++ shoppinglist.96546a43.js
@@ -0,0 +1,2 @@
+var e=window.location.pathname,s=document.querySelector(".home-link"),t=document.querySelector(".shopping-list-link");"/index.html"===e?(s.classList.add("current-page-home"),t.classList.remove("current-page-shopping")):"/shoppinglist.html"===e&&(t.classList.add("current-page-shopping"),s.classList.remove("current-page-home"));
+//# sourceMappingURL=shoppinglist.96546a43.js.map
